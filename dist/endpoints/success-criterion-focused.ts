87a10d6d95cd76700f939a133a4cdf3d58058625
Grunet <sadikshahidain@gmail.com>
2020-06-17 21:12:21 -0500

    Adding in the real implementation

--- dist/endpoints/success-criterion-focused.ts
+++ dist/endpoints/success-criterion-focused.ts
@@ -8,14 +8,24 @@ async function getTemplateHtml(): Promise<string> {
   // );
 
   // return resBody;
-  if (import.meta.url.startsWith("http")) {
-    throw Error(import.meta.url);
-  }
-
   const pathToHtml = import.meta.url
     .replace("endpoints", "templates")
     .replace(".ts", ".html");
-  const html = await Deno.readTextFile(pathToHtml);
+
+  let html: string;
+  if (import.meta.url.startsWith("http")) {
+    const res = await fetch(pathToHtml);
+
+    html = new TextDecoder("utf-8").decode(
+      new Uint8Array(await res.arrayBuffer()),
+    );
+  } else if (import.meta.url.startsWith("file")) {
+    html = await Deno.readTextFile(pathToHtml);
+  } else {
+    throw Error(
+      "import.meta.url doesn't seem to point to a networked resource or a local file",
+    );
+  }
 
   return html;
 }
