d8c744538e295ab1983c1ce934d98bc1fd3902da
Grunet <sadikshahidain@gmail.com>
2020-06-18 20:10:25 -0500

    Adding a task to run the tests

--- scripts/copy.ts
+++ scripts/copy.ts
@@ -1,4 +1,3 @@
-// Have to use the "--unstable" flag for this to work. See https://github.com/denoland/deno/issues/5175
 import { copy } from "https://deno.land/std@0.57.0/fs/copy.ts";
 
 await copy("../src/", "../dist/", { overwrite: true });
