60b3ff79498bee1b544f7681c81bf17b96fbb9f5
Grunet <sadikshahidain@gmail.com>
2020-11-22 11:11:36 -0600

    Updating the copy module from the standard library to a version that works with the latest Deno version (1.5.3)

--- scripts/copy.ts
+++ scripts/copy.ts
@@ -1,3 +1,3 @@
-import { copy } from "https://deno.land/std@0.57.0/fs/copy.ts";
+import { copy } from "https://deno.land/std@0.78.0/fs/copy.ts";
 
 await copy("../src/", "../dist/", { overwrite: true });
